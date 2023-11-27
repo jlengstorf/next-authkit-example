27a2d9f2645799672f261170ca64785f804b3b4f
Jason Lengstorf <jason@lengstorf.com>
2023-11-27 15:55:46 -0800

    feat: roll back middleware for tutorial

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,21 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
-import { getAuthorizationUrl, verifyJwtToken } from "./auth";
 
 export async function middleware(request: NextRequest) {
-  const { cookies } = request;
-  const { value: token } = cookies.get("token") ?? { value: null };
-
-  const hasVerifiedToken = token && (await verifyJwtToken(token));
-
-  // Redirect unauthenticated users to the AuthKit flow
-  if (!hasVerifiedToken) {
-    const authorizationUrl = await getAuthorizationUrl();
-    const response = NextResponse.redirect(authorizationUrl);
-
-    response.cookies.delete("token");
-
-    return response;
-  }
+  // TODO add handling for logged in/logged out users
 
   return NextResponse.next();
 }
