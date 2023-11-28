221ef620b1448210bad46179f8210af4a5390d99
Jason Lengstorf <jason@lengstorf.com>
2023-11-27 16:14:51 -0800

    feat: rollback callback further for tutorial

--- src/app/callback/route.ts
+++ src/app/callback/route.ts
@@ -2,31 +2,7 @@ import { SignJWT } from "jose";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
-  const code = request.nextUrl.searchParams.get("code");
-
-  if (code) {
-    try {
-      // TODO Use the code returned to us by AuthKit and authenticate the user with WorkOS
-
-      // TODO Create a JWT token with the user's information
-
-      const url = request.nextUrl.clone();
-
-      // TODO Cleanup params
-
-      // Redirect to the requested path and store the session
-      url.pathname = "/";
-      const response = NextResponse.redirect(url);
-
-      // TODO set a cookie with the user details
-
-      return response;
-    } catch (error) {
-      return NextResponse.json(error);
-    }
-  }
-
   return NextResponse.json({
-    error: "No authorization code was received from AuthKit",
+    error: "TODO: implement AuthKit callback flow",
   });
 }
